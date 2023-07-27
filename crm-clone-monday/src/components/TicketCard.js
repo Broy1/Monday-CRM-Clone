cabd65bbfc70e03875467411164e1c1d6890079c
Broy1 <roy.bogya@gmail.com>
2023-07-28 01:28:13 +0200

    components created

---
+++ crm-clone-monday/src/components/TicketCard.js
@@ -0,0 +1,15 @@
+import AvatarDisplay from "./AvatarDisplay"
+import StatusDisplay from "./StatusDisplay"
+import PriorityDisplay from "./PriorityDisplay"
+import ProgressDisplay from "./ProgessDisplay"
+import DeleteBlock from "./DeleteBlock"
+
+const TicketCard = () =>{
+    return (
+        <div className="ticket-card">
+
+        </div>
+    )
+}
+
+export default TicketCard
