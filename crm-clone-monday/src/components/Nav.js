efe8f8065dbc421a34d4052cf7910eeaf8d58598
Broy1 <roy.bogya@gmail.com>
2023-07-28 01:28:29 +0200

    navbar update

--- crm-clone-monday/src/components/Nav.js
+++ crm-clone-monday/src/components/Nav.js
@@ -1,6 +1,21 @@
+import logo from '../images/venom_logo.png'
+import { useNavigate } from 'react-router-dom'
+
 const Nav = () =>{
+
+    const navigate = useNavigate()
+
     return (
-        <div>Nav</div>
+        <nav>
+            <div className="logo-container">
+                <img src={logo} alt="logo" />
+            </div>
+            <div className="controls-container">
+                {/* navigate using the react dom */}
+                <div className='icon' onClick={()=> navigate('/ticket')}>+</div>
+                <div className='icon' onClick={()=> navigate('/')}>Home</div>
+            </div>
+        </nav>
     )
 }
 
