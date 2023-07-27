77b1173e6faa432b5f49365e8dd5e2800f696dc7
Broy1 <roy.bogya@gmail.com>
2023-07-28 01:28:38 +0200

    app.js update

--- crm-clone-monday/src/App.js
+++ crm-clone-monday/src/App.js
@@ -1,12 +1,20 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Nav from './components/Nav'
 import Dashboard from './pages/Dashboard';
+import TicketPage from './pages/TicketPage';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      {/* nav here so no matter the route its displayed */}
+      <Nav />
         <Routes>
+          {/* if we go to the / page we get dashboard*/}
           <Route path='/' element={<Dashboard/>}/>
+          <Route path='/ticket' element={<TicketPage/>}/>
+          {/* we pass some id, enable edit mode */}
+          <Route path='/ticket/:id' element={<TicketPage editMode={true}/>}/>
         </Routes>
       </BrowserRouter>
     </div>
