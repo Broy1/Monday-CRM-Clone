07a59d87a82dc7bfe125a5962ed9a7185eb464ce
Broy1 <roy.bogya@gmail.com>
2023-07-31 15:14:35 +0200

    avatar display implementation

--- crm-clone-monday/src/components/TicketCard.js
+++ crm-clone-monday/src/components/TicketCard.js
@@ -3,11 +3,21 @@ import StatusDisplay from "./StatusDisplay"
 import PriorityDisplay from "./PriorityDisplay"
 import ProgressDisplay from "./ProgessDisplay"
 import DeleteBlock from "./DeleteBlock"
+import {Link} from "react-router-dom"
 
-const TicketCard = () =>{
+const TicketCard = ({color, ticket}) =>{
     return (
         <div className="ticket-card">
-
+            
+            <Link to={'/ticket/${ticket.documentId}'} id="link">
+                <div className="ticket-color"></div>
+                <h3>{ticket.title}</h3>
+                <AvatarDisplay ticket={ticket}/>
+                <StatusDisplay/>
+                <PriorityDisplay/>
+                <ProgressDisplay/>
+            </Link>
+            <DeleteBlock/>
         </div>
     )
 }
