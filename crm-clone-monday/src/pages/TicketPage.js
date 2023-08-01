bad3e2b3bafd095dd342a951ae87e34ffdbd80fb
Broy1 <roy.bogya@gmail.com>
2023-08-01 17:20:48 +0200

    ticketpage update

--- crm-clone-monday/src/pages/TicketPage.js
+++ crm-clone-monday/src/pages/TicketPage.js
@@ -1,8 +1,21 @@
+import {useState} from 'react'
+
 const TicketPage = () =>{
+    
+    const [formData, setFormData] = useState({
+        status: 'not started',
+        progress: 0,
+        timestamp: new Date().toISOString()
+    })
 
     const editMode = false;
+
     const handleSubmit = () => {
         console.log('submitted')
+    }
+
+    const handleChange = () => {
+        console.log('changed')
     }
 
     return (
@@ -10,7 +23,17 @@ const TicketPage = () =>{
             <h1>{editMode ? 'Update your ticket':'Create a ticket'}</h1>
             <div className="ticket-container">
                 <form onSubmit={handleSubmit}>
-
+                     <section>
+                        <label htmlFor="title">Title</label>
+                        <input
+                            id="title"
+                            name="title"
+                            type="text"
+                            onChange={handleChange}
+                            required={true}
+                            value={FormData.title}
+                        />
+                     </section>
                 </form>
             </div>
         </div>
